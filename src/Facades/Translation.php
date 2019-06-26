408f4ad319bae26132d35e06842ab9c667562d66
Ahmed Gad <eng.ahmedmgad@gmail.com>
2019-06-26 13:42:13 +0200

    dynamic routes options

--- src/Facades/Translation.php
+++ src/Facades/Translation.php
@@ -2,8 +2,11 @@
 namespace Motwreen\Translation\Facades;
 
 use Motwreen\Translation\Translation;
-
 use Illuminate\Support\Facades\Facade;
+/**
+ * @method static routes(array $options)
+ * @see \Motwreen\Translation\Translation;
+ */
 
 class Translation extends Facade
 {
--- src/Translation.php
+++ src/Translation.php
@@ -5,12 +5,16 @@ use Illuminate\Support\Facades\Route;
 
 class Translation
 {
-    public static function routes(array $group = [])
+	/**
+     * @param array $options
+     * @return void
+     */
+    public static function routes(array $options = [])
     {
        $packageGroup = ['namespace' => 'Motwreen\Translation\Http\Controllers', 'middleware' => ['web']];
        $mergedGroups = array_merge($packageGroup,$group);
       
-		Route::group($mergedGroups], function() {
+		Route::group($mergedGroups, function() {
 		    Route::get('translation/ajax_validate/file_name', 'TranslationController@validateNewFileName')->name('translation.validate_file_name');
 		    Route::get('translation/ajax_read', 'TranslationController@readLangFileAjax')->name('translation.ajax_read_file');
 		    Route::post('translation/{locale}/', 'TranslationController@saveTranslations')->name('translation.save_translations');
