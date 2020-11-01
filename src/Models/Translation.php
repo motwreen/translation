6aec1d795c8c3c5eddfee141fee3a6f90a4c86fd
Ahmed Gad <eng.ahmedmgad@gmail.com>
2020-11-01 14:13:51 +0200

    Update Translation.php

--- src/Models/Translation.php
+++ src/Models/Translation.php
@@ -1,10 +1,13 @@
 <?php
 namespace Motwreen\Translation\Models;
 
+use Hyn\Tenancy\Traits\UsesTenantConnection;
 use Illuminate\Database\Eloquent\Model;
 
 class Translation extends Model
 {
+    use UsesTenantConnection;
+
     public function locale()
     {
         return $this->belongsTo(Locale::class,'locale_id');
