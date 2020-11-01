3c30ddd7fdb037d8a2b1d5a9e1801db982807289
Ahmed Gad <eng.ahmedmgad@gmail.com>
2020-11-01 14:13:53 +0200

    Update Locale.php

--- src/Models/Locale.php
+++ src/Models/Locale.php
@@ -1,11 +1,12 @@
 <?php
 namespace Motwreen\Translation\Models;
 
+use Hyn\Tenancy\Traits\UsesTenantConnection;
 use Illuminate\Database\Eloquent\Model;
 
 class Locale extends Model
 {
-
+    use UsesTenantConnection;
     public function getDefaultAttribute()
     {
         return $this->iso == config('app.locale');
